68c2c5b0428b0ac197bd821cfd4203a049e42781
1065120995 <1065120995@qq.com>
2018-08-16 17:44:30 +0800

    添加:首页增加

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,8 +1,8 @@
 /*
  2018/8/14 孙
  */
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams, Slides} from 'ionic-angular';
 
 
 @IonicPage()
@@ -10,14 +10,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-dashboard',
   templateUrl: 'dashboard.html',
 })
+
 export class DashboardPage {
+  @ViewChild(Slides) slides: Slides;
   searchContent:string;
+  bannerList = [];
   constructor(public navCtrl: NavController,
               public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     this.searchContent = "蚂蚁森林";
+    this.bannerList.push({src:"assets/imgs/banner1.png"});
+    this.bannerList.push({src:"assets/imgs/banner2.png"});
+    this.bannerList.push({src:"assets/imgs/banner3.png"});
+    this.bannerList.push({src:"assets/imgs/banner4.png"});
   }
 
 }
